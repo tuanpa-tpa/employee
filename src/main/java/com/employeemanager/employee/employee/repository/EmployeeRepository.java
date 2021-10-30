3b31be6d788e9ff44401fb1a18db839dfc9482d9
PAT <tuanpa.tpa@gmail.com>
2021-10-30 23:40:56 +0700

    create employee repository

---
+++ src/main/java/com/employeemanager/employee/employee/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.employeemanager.employee.employee.repository;
+
+import com.employeemanager.employee.employee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
