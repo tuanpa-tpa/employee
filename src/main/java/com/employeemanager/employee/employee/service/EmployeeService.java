d52f6f23b915a3a1a1e280a6e525148deef29511
PAT <tuanpa.tpa@gmail.com>
2021-10-30 23:41:17 +0700

    create employee service

---
+++ src/main/java/com/employeemanager/employee/employee/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.employeemanager.employee.employee.service;
+
+import com.employeemanager.employee.employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> findAll();
+}
---
+++ src/main/java/com/employeemanager/employee/employee/service/EmployeeServiceImpl.java
@@ -0,0 +1,23 @@
+package com.employeemanager.employee.employee.service;
+
+import com.employeemanager.employee.employee.model.Employee;
+import com.employeemanager.employee.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+}
