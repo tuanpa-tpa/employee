834763d45c2e331ad6127c99020ddb0827e16d59
PAT <tuanpa.tpa@gmail.com>
2021-10-30 23:40:12 +0700

    create model employee

---
+++ src/main/java/com/employeemanager/employee/employee/model/Employee.java
@@ -0,0 +1,26 @@
+package com.employeemanager.employee.employee.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "country")
+    private String country;
+}
